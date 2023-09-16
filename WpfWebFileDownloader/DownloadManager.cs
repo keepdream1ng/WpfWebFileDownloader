dba34a0dfb79ae331fdd6c71a21761dff3d0925b
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-16 16:36:45 +0530

    basic functionality acchived

---
+++ WpfWebFileDownloader/DownloadManager.cs
@@ -0,0 +1,39 @@
+﻿using AltoHttp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfWebFileDownloader
+{
+    public class DownloadManager
+    {
+        public string DownloadLink { get; set; }
+        public string FileName { get { return Path.GetFileName(DownloadLink); } }
+        public string SavePath { get; set; }
+
+        private string _tempFilePath;
+        public HttpDownloader client { get; private set; }
+
+        public void DownloadFile()
+        {
+            if (client != null)
+            {
+                client.StopReset();
+            }
+            _tempFilePath = Path.Combine(SavePath, Guid.NewGuid().ToString());
+            client = new HttpDownloader(DownloadLink, _tempFilePath);
+            client.Start();
+            return;
+        }
+
+        public async Task CancelDownload()
+        {
+            client.Pause();
+        }
+    }
+}
--- WpfWebFileDownloader/MainWindow.xaml.cs
+++ WpfWebFileDownloader/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,16 +22,76 @@ namespace WpfWebFileDownloader
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DownloadManager downloadManager;
         public MainWindow()
         {
             InitializeComponent();
+            downloadManager = new DownloadManager();
             InitialSetUp();
         }
 
         private void InitialSetUp()
         {
-            Download_Button.IsEnabled = false;
+            //Download_Button.IsEnabled = false;
             Link_TextBox.Text = "https://download.sprutcam.com/links/SprutCAM_X.zip";
+            SavePath_TextBox.Text = "D:\\test";
+        }
+
+        private void OnProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            Download_ProgressBar.Value = (int)e.Progress;
+        }
+
+        private void OnDownloadCompleted(object? sender, EventArgs e)
+        {
+            Download_Button.IsEnabled = true;
+            MessageBox.Show("File Download Complete!");
+        }
+
+        private void Link_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            downloadManager.DownloadLink = Link_TextBox.Text;
+        }
+
+        private void SavePath_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            downloadManager.SavePath = SavePath_TextBox.Text;
+        }
+
+        private void Download_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Download_Button.IsEnabled = false;
+            ResetProgressBar();
+            downloadManager.DownloadFile();
+            downloadManager.client.DownloadCompleted += OnDownloadCompleted;
+            downloadManager.client.ProgressChanged += OnProgressChanged;
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            downloadManager.CancelDownload();
+            ResetProgressBar();
+            Download_Button.IsEnabled = true;
+        }
+
+        private void ResetProgressBar()
+        {
+            Download_ProgressBar.Value = 0;
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if ((downloadManager.client.State == AltoHttp.Status.Downloading)
+                ||
+                (downloadManager.client.State == AltoHttp.Status.SendingRequest))
+            {
+                MessageBox.Show("Stop the download before closing window!");
+                e.Cancel = true;
+            }
+            else
+            {
+                downloadManager.client.StopReset();
+            }
         }
     }
 }
